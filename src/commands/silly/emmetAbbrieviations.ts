77f1d7c7d96e51da0cfe477e8ed01fef6403aca6
CatHat <liase810@gmail.com>
2024-09-30 22:51:30 +0200

    fixed ts error

--- src/commands/silly/emmetAbbrieviations.ts
+++ src/commands/silly/emmetAbbrieviations.ts
@@ -2,19 +2,18 @@ import { CommandInteraction, SlashCommandBuilder } from 'discord.js'
 import expand from 'emmet'
 
 export const data = new SlashCommandBuilder()
-  .setName('emmetabbrieviations')
-  .setDescription('Adds the emmet abbrieviation functionality to discord')
-  .addStringOption((option) =>
-    option
-      .setName('codeblock')
-      .setDescription('The code block to parse')
-      .setRequired(true)
-  )
+    .setName('emmetabbrieviations')
+    .setDescription('Adds the emmet abbrieviation functionality to discord')
+    .addStringOption((option) =>
+        option
+            .setName('codeblock')
+            .setDescription('The code block to parse')
+            .setRequired(true)
+    )
 
 export async function execute(interaction: CommandInteraction) {
-  const content = interaction.options.get('codeblock', true).value.toString()
+    const content = interaction.options.get('codeblock', true).value.toString()
+    if (!content) return
 
-  return await interaction.reply(
-    `\`\`\`html\n${expand(content)}\`\`\``
-  )
+    return await interaction.reply(`\`\`\`html\n${expand(content)}\`\`\``)
 }
